c55920ccdd921d65f721b279be8c7dbddd24b4fb
Virginia <viorbe20@gmail.com>
2024-06-25 18:24:10 +0200

    add nlp section

---
+++ js/templates.js
@@ -0,0 +1,16 @@
+// Load the navigation bar
+// fetch("./templates/nav.html")
+//     .then((response) => response.text())
+//     .then((html) => {
+//         document.getElementById("nav-container").innerHTML = html;
+//     })
+//     .catch((error) => console.error("Error al cargar el nav:", error));
+
+Promise.all([
+    fetch("./templates/nav.html").then((response) => response.text()),
+    fetch("./templates/nlp.html").then((response) => response.text()),
+]).then((html) => {
+    document.getElementById("nav-container").innerHTML = html[0];
+    document.getElementById("nlp-description").innerHTML = html[1];
+}).catch((error) => console.error("Error al cargar el archivo:", error));
+
