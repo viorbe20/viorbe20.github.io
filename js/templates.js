8b26f26669012a6baa9c54eae860006b3cde1375
Virginia <viorbe20@gmail.com>
2024-06-26 12:48:02 +0200

    add daw graph

--- js/templates.js
+++ js/templates.js
@@ -11,10 +11,12 @@ Promise.all([
     fetch("./templates/nlp.html").then((response) => response.text()),
     fetch("./templates/machine-learning.html").then((response) => response.text()),
     fetch("./templates/big-data.html").then((response) => response.text()),
+    fetch("./templates/daw.html").then((response) => response.text()),
 ]).then((html) => {
     document.getElementById("nav-container").innerHTML = html[0];
     document.getElementById("nlp-description").innerHTML = html[1];
     document.getElementById("machine-learning-description").innerHTML = html[2];
     document.getElementById("big-data-description").innerHTML = html[3];
+    document.getElementById("daw-description").innerHTML = html[4];
 }).catch((error) => console.error("Error al cargar el archivo:", error));
 
