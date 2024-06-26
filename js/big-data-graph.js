2d931f3478b8409273bea261d1d3a275da49b1e3
Virginia <viorbe20@gmail.com>
2024-06-26 12:33:28 +0200

    add big data info

---
+++ js/big-data-graph.js
@@ -0,0 +1,62 @@
+console.log("Loading big-data-graph.js");
+am5.ready(function() {
+
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("big-data-graph");
+    
+    
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+    
+    
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+    var chart = root.container.children.push(am5percent.PieChart.new(root, {
+      layout: root.verticalLayout
+    }));
+    
+    
+    // Create series
+    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+    var series = chart.series.push(am5percent.PieSeries.new(root, {
+      valueField: "value",
+      categoryField: "category"
+    }));
+    
+    
+    // Set data
+    // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+    series.data.setAll([
+        { value: 10, category: "Kafka" },
+        { value: 9, category: "Hadoop" },
+        { value: 6, category: "Spark" },
+        { value: 5, category: "Neo4j" },
+        { value: 4, category: "MongoDB" },
+        { value: 3, category: "PowerBI" },
+        { value: 1, category: "Tableau" },
+        { value: 1, category: "Databricks"},
+        { value: 1, category: "Hive" },
+    ]);
+    
+    
+    // Create legend
+    // https://www.amcharts.com/docs/v5/charts/percent-charts/legend-percent-series/
+    var legend = chart.children.push(am5.Legend.new(root, {
+      centerX: am5.percent(50),
+      x: am5.percent(50),
+      marginTop: 15,
+      marginBottom: 15
+    }));
+    
+    legend.data.setAll(series.dataItems);
+    
+    
+    // Play initial series animation
+    // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
+    series.appear(1000, 100);
+    
+    }); // end am5.ready()
--- js/templates.js
+++ js/templates.js
@@ -10,9 +10,11 @@ Promise.all([
     fetch("./templates/nav.html").then((response) => response.text()),
     fetch("./templates/nlp.html").then((response) => response.text()),
     fetch("./templates/machine-learning.html").then((response) => response.text()),
+    fetch("./templates/big-data.html").then((response) => response.text()),
 ]).then((html) => {
     document.getElementById("nav-container").innerHTML = html[0];
     document.getElementById("nlp-description").innerHTML = html[1];
     document.getElementById("machine-learning-description").innerHTML = html[2];
+    document.getElementById("big-data-description").innerHTML = html[3];
 }).catch((error) => console.error("Error al cargar el archivo:", error));
 
