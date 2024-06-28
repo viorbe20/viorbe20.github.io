e712a28315be1b8ed4bc68d10c0470ea21228bff
Virginia <viorbe20@gmail.com>
2024-06-28 13:01:21 +0200

    add education part 1

--- js/templates.js
+++ js/templates.js
@@ -4,7 +4,8 @@ Promise.all([
     fetch("./templates/machine-learning.html").then((response) => response.text()),
     fetch("./templates/big-data.html").then((response) => response.text()),
     fetch("./templates/daw.html").then((response) => response.text()),
-    fetch("./templates/projects-ia.html").then((response) => response.text())
+    fetch("./templates/projects-ia.html").then((response) => response.text()),
+    fetch("./templates/education-oficial.html").then((response) => response.text())
 ]).then((html) => {
     document.getElementById("nav-container").innerHTML = html[0];
     document.getElementById("nlp-description").innerHTML = html[1];
@@ -12,5 +13,6 @@ Promise.all([
     document.getElementById("big-data-description").innerHTML = html[3];
     document.getElementById("daw-description").innerHTML = html[4];
     document.getElementById("projects-cards-ia").innerHTML = html[5];
+    document.getElementById("education-oficial-container").innerHTML = html[6];
 }).catch((error) => console.error("Error al cargar el archivo:", error));
 
